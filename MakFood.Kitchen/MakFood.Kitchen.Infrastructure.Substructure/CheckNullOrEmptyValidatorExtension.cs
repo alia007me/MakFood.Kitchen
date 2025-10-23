6ddd620b786d60bba20ec75e422a9145afca23c1
Your Name <your.email@example.com>
2025-10-23 22:37:58 +0330

    Feach: اضافه کردن اکستنشن متد جنریک برای تشخیص نال یا امپتی بودن ورودی

---
+++ MakFood.Kitchen/MakFood.Kitchen.Infrastructure.Substructure/CheckNullOrEmptyValidatorExtension.cs
@@ -0,0 +1,29 @@
+﻿namespace MakFood.Kitchen.Infrastructure.Substructure
+{
+    public static class CheckNullOrEmptyValidatorExtension
+    {
+        public static void CheckNullOrEmpty<T>(this T input, string pName)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(pName); 
+            }
+
+            if (input is string s && string.IsNullOrEmpty(s))
+            {
+                throw new ArgumentException("String argument is empty", pName);
+            }
+
+            if (input is DateOnly dt && dt == default)
+            {
+                throw new ArgumentException("Date argument is empty",pName);
+            }
+
+            if (input is uint uin && uin == 0)
+            {
+                throw new ArgumentException("Uint argument is empty", pName);
+            }
+            
+        }
+    }
+}
