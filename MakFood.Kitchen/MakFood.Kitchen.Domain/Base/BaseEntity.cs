9320d080b430e50c418b2d1168ef5a6456f17b58
Your Name <your.email@example.com>
2025-10-23 14:32:42 +0330

    Structure: ایجاد انتیتی بیس و دیپندنسی ها

---
+++ MakFood.Kitchen/MakFood.Kitchen.Domain/Base/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace MakFood.Kitchen.Domain.Base
+{
+    public abstract class BaseEntity<TId>
+    {
+        protected BaseEntity()
+        {
+            CreationDateTime = DateTime.Now;
+        }
+
+        public TId Id { get; protected set; }
+        public DateTime CreationDateTime { get; private set; }
+    }
+}
