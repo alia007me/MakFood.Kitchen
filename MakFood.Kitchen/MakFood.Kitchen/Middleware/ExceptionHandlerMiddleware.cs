4d42018059447f554f0ec4a90059a8830507f216
abyz2704 <zahratajarrob82@gmail.com>
2025-11-15 13:25:36 +0330

    middleware _ excptionHandler

---
+++ MakFood.Kitchen/MakFood.Kitchen/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using MakFood.Kitchen.Infrastructure.Substructure.Exceptions;
+using System.Net;
+
+namespace MakFood.Kitchen.Middleware
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+
+            catch (ValidationFailedDomainException exception)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                await context.Response.WriteAsync(exception.Message);
+            }
+
+            catch (ForbbidenDomainException exception)
+            {
+                context.Response.StatusCode = (int)(HttpStatusCode.Forbidden);
+                await context.Response.WriteAsync(exception.Message);
+            }
+
+            catch (Exception exception)
+            {
+                context.Response.StatusCode = (int)((HttpStatusCode)HttpStatusCode.InternalServerError);
+                await context.Response.WriteAsync("An unexpected server error occurred.");
+            }
+        }
+    }
+}
---
+++ MakFood.Kitchen/MakFood.Kitchen/Middleware/ExceptionHandlerMiddlewareExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace MakFood.Kitchen.Middleware
+{
+    public static class ExceptionHandlerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandlerMiddleware(this IApplicationBuilder builder)
+        {
+            
+            return builder.UseMiddleware<ExceptionHandlerMiddleware>();
+        }
+    }
+}
