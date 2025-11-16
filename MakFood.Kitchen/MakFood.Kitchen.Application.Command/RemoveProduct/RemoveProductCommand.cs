ea1c0fda7e816a501f3af61700c38a0c0e906577
saman salehi <samansalehi20051384@gmail.com>
2025-11-16 07:45:26 -0800

    feat : اضافه کردن کامند و کامند ولیدیشن

---
+++ MakFood.Kitchen/MakFood.Kitchen.Application.Command/RemoveProduct/RemoveProductCommand.cs
@@ -0,0 +1,22 @@
+﻿using MakFood.Kitchen.Application.Command.AddProduct;
+using MakFood.Kitchen.Application.Command.CommandBases;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakFood.Kitchen.Application.Command.RemoveProduct
+{
+    public class RemoveProductCommand : CommandBase , IRequest<bool>
+    {
+
+        public Guid ProductId { get; set; }
+
+        public override void Validate()
+        {
+            new RemoveProductCommandValidation().Validate(this);
+        }
+    }
+}
---
+++ MakFood.Kitchen/MakFood.Kitchen.Application.Command/RemoveProduct/RemoveProductCommandValidation.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace MakFood.Kitchen.Application.Command.RemoveProduct
+{
+    public class RemoveProductCommandValidation : AbstractValidator<RemoveProductCommand>
+    {
+        public RemoveProductCommandValidation()
+        {
+            RuleFor(x => x.ProductId).NotEmpty()
+           .WithMessage("The Name is Not Empty");
+        }
+    }
+}
