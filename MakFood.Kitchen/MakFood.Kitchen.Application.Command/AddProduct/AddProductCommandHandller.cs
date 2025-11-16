b9601371105f8c8a331ed013575a1def498b38ea
saman salehi <samansalehi20051384@gmail.com>
2025-11-16 07:24:56 -0800

    feat : اضافه کردن کامند هندلر و کنترلر

---
+++ MakFood.Kitchen/MakFood.Kitchen.Application.Command/AddProduct/AddProductCommandHandller.cs
@@ -0,0 +1,47 @@
+﻿using MakFood.Kitchen.Domain.Entities.CategoryAggrigate.Contract;
+using MakFood.Kitchen.Domain.Entities.ProductAggrigate;
+using MakFood.Kitchen.Domain.Entities.ProductAggrigate.Contract;
+using MakFood.Kitchen.Infrastructure.Persistence.Context;
+using MakFood.Kitchen.Infrastructure.Persistence.Context.UnitOfWorks;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakFood.Kitchen.Application.Command.AddProduct
+{
+    internal class AddProductCommandHandller : IRequestHandler<AddProductCommand, AddProductCommandResponce>
+    {
+        public readonly ApplicationDbContext _context;
+        public readonly IProductRepository productRepository;
+        public readonly ISubCategoryRepository subCategoryRepository;
+        public readonly IUnitOfWork unitOfWork;
+
+        public AddProductCommandHandller(ApplicationDbContext context, IProductRepository productRepository, ISubCategoryRepository subCategoryRepository, IUnitOfWork unitOfWork)
+        {
+            _context = context;
+            this.productRepository = productRepository;
+            this.subCategoryRepository = subCategoryRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<AddProductCommandResponce> Handle(AddProductCommand request, CancellationToken cancellationToken)
+        {
+            var subCategory = await subCategoryRepository.GetSubCategoryBySabCategoryNameAsync(request.SubCategoryName);
+            var product = new Product
+                (
+                    request.Name,
+                    request.Price,
+                    request.Description,
+                    request.ThumbnailPath,
+                    subCategory,
+                    request.AvailableQuantity
+                );
+            await productRepository.AddProductAsync(product);
+            await unitOfWork.SaveChangesAsync();
+            return new AddProductCommandResponce(product.Id);
+        }
+    }
+}
---
+++ MakFood.Kitchen/MakFood.Kitchen/Controller/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MakFood.Kitchen.Application.Command.AddProduct;
+
+namespace MakFood.Kitchen.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost("Kitchen/{CastomerId}/Product/AddProduct")]
+        public async Task<IActionResult> AddProduct( AddProductCommand cammand)
+        {
+            var w = await _mediator.Send(cammand);
+            return Ok(w);
+        }
+    }
+}
