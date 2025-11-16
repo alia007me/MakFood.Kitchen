3d2fbc367e59dbe30ec3d95e255048b5b7d3c39e
saman salehi <samansalehi20051384@gmail.com>
2025-11-16 09:48:13 -0800

    feat p : اضافه کردن کامند و ولیدیشن اپدیت کردن پروداکت

---
+++ MakFood.Kitchen/MakFood.Kitchen.Application.Command/UpdateProduct/UpdateProductCommand.cs
@@ -0,0 +1,29 @@
+﻿using MakFood.Kitchen.Application.Command.AddProduct;
+using MakFood.Kitchen.Application.Command.CommandBases;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakFood.Kitchen.Application.Command.UpdateProduct
+{
+    public class UpdateProductCommand : CommandBase, IRequest<bool>
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string ThumbnailPath { get; set; }
+        public Guid SubCategoryId { get; set; }
+        public string SubCategoryName { get; set; }
+        public uint AvailableQuantity { get; set; }
+
+        public override void Validate()
+        {
+            new UpdateProductCommandValidation().Validate(this);
+        }
+    }
+
+
+}
